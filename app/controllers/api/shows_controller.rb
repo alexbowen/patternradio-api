88c55ffc006cde821706ce3f55e7c600d2fd2c21
alexbowen <alexbowen123@googlemail.com>
2024-02-16 00:31:22 +0000

    seperate external/internal shows

--- app/controllers/api/shows_controller.rb
+++ app/controllers/api/shows_controller.rb
@@ -2,16 +2,33 @@ class Api::ShowsController < ApplicationController
   def index
     if params[:q].present? && params[:q].length > 0
       shows = Show.order("created_time DESC").search(params[:q])
-      count = shows.length
-      items = shows.slice(params[:offset].to_i, params[:limit].to_i).reverse
-    elsif params[:filters].present? && params[:filters].length > 0
-      shows = Show.order("created_time DESC").select { |show| show[:tags].any?{|tag| params[:filters].split(",").any? { |filter| filter.in?(tag["name"].downcase) } } }
-      count = shows.length
-      items = shows.slice(params[:offset].to_i, params[:limit].to_i).reverse
+    elsif params[:creator].present? && params[:creator] == "external"
+      if params[:filters].present? && params[:filters].length > 0
+        shows = Show.order("created_time DESC").select { |show| show[:user]["username"] != "patternradio"}.select { |show| show[:tags].any?{|tag| params[:filters].split(",").any? { |filter| filter.in?(tag["name"].downcase) } } }
+      else
+        shows = Show.order("created_time DESC").select { |show| show[:user]["username"] != "patternradio"}
+      end
+      
+    elsif params[:creator].present? && params[:creator] == "internal"
+      if params[:filters].present? && params[:filters].length > 0
+        shows = Show.order("created_time DESC").select { |show| show[:user]["username"] == "patternradio"}.select { |show| show[:tags].any?{|tag| params[:filters].split(",").any? { |filter| filter.in?(tag["name"].downcase) } } }
+      else
+        shows = Show.order("created_time DESC").select { |show| show[:user]["username"] == "patternradio"}
+      end
     else
-      items = Show.order("created_time DESC").limit(params[:limit]).offset(params[:offset]).reverse
-      count = Show.count
+      if params[:filters].present? && params[:filters].length > 0
+      shows = Show.order("created_time DESC").select { |show| show[:tags].any?{|tag| params[:filters].split(",").any? { |filter| filter.in?(tag["name"].downcase) } } }
+      else
+        shows = Show.order("created_time DESC")
+      end
     end
+
+    # if params[:filters].present? && params[:filters].length > 0
+    #   shows = shows.select { |show| show[:tags].any?{|tag| params[:filters].split(",").any? { |filter| filter.in?(tag["name"].downcase) } } }
+    # end
+
+    count = shows.length
+    items = shows.slice(params[:offset].to_i, params[:limit].to_i)
 
     render json: { count: count, items: items }
   end
